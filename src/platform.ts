9902d7a66510317b97470b27536822764a6df20b
Graham Hargreaves <grahamhar@users.noreply.github.com>
2023-10-16 21:48:53 +0100

    Initial commit

---
+++ src/platform.ts
@@ -0,0 +1,137 @@
+import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+
+
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import { GivEnergyInverterAccessory } from './platformAccessory';
+import axios from 'axios';
+
+interface battery {
+  percent: number;
+  power: number;
+}
+
+interface grid {
+  power: number;
+}
+interface solar {
+  power: number;
+}
+
+interface GivEnergyInverter {
+  serialNumber: string;
+  displayName: string;
+  model: string;
+  battery: battery;
+  grid: grid;
+  solar: solar;
+}
+/**
+ * HomebridgePlatform
+ * This class is the main constructor for your plugin, this is where you should
+ * parse the user config and discover/register accessories with Homebridge.
+ */
+export class GivEnergyPlugin implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service = this.api.hap.Service;
+  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+
+  // this is used to track restored cached accessories
+  public readonly accessories: PlatformAccessory[] = [];
+
+  constructor(
+    public readonly log: Logger,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      // run the method to discover / register your devices as accessories
+      this.discoverDevices();
+    });
+  }
+
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+
+    // add the restored accessory to the accessories cache so we can track if it has already been registered
+    this.accessories.push(accessory);
+  }
+
+  /**
+   * This is an example method showing how to register discovered accessories.
+   * Accessories must only be registered once, previously created accessories
+   * must not be registered again to prevent "duplicate UUID" errors.
+   */
+  async discoverDevices() {
+
+    const serialNumbers = await axios.get('https://api.givenergy.cloud/v1/communication-device', {headers: {
+      'Authorization': 'Bearer ' + this.config.API_KEY,
+      'accept': 'application/json',
+    }, timeout: 5000}).then( response => {
+      const serials: any = [];
+      response.data.data.forEach(inverter => {
+        this.log.info('Found inverter: ' + JSON.stringify(inverter.inverter));
+        this.log.info('Found inverter: ' + inverter.inverter.serial);
+        serials.push({serial: inverter.inverter.serial, model: inverter.inverter.info.model});
+      });
+      return serials;
+    }).catch(error => {
+      this.log.error(error);
+    });
+    await serialNumbers.forEach(inverter => {
+      axios.get(`https://api.givenergy.cloud/v1/inverter/${inverter.serial}/system-data/latest`, {headers: {
+        'Authorization': 'Bearer ' + this.config.API_KEY,
+        'accept': 'application/json',
+      }, timeout: 5000}).then(response => {
+        this.log.info('Found inverter details: ' + JSON.stringify(response.data));
+        const givInverter: GivEnergyInverter = {
+          serialNumber: inverter.serial,
+          displayName: inverter.serial,
+          model: inverter.model,
+          battery: {
+            percent: response.data.data.battery.percent,
+            power: response.data.data.battery.power,
+          },
+          grid: {
+            power: response.data.data.grid.power,
+          },
+          solar: {
+            power: response.data.data.solar.power,
+          },
+        };
+        const uuid = this.api.hap.uuid.generate(givInverter.serialNumber);
+        const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+        if (existingAccessory) {
+
+          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+
+          new GivEnergyInverterAccessory(this, existingAccessory);
+
+        } else {
+          this.log.info('Adding new accessory:', givInverter.displayName);
+          this.log.info('Adding new accessory:', JSON.stringify(givInverter));
+
+          const accessory = new this.api.platformAccessory(givInverter.displayName, uuid);
+
+          accessory.context.device = givInverter;
+
+          new GivEnergyInverterAccessory(this, accessory);
+
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        }
+      }).catch(error => {
+        this.log.error(error);
+      });
+    });
+
+  }
+}
---
+++ src/platformAccessory.ts
@@ -0,0 +1,160 @@
+import { Service, PlatformAccessory } from 'homebridge';
+
+import { GivEnergyPlugin } from './platform';
+import axios from 'axios';
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class GivEnergyInverterAccessory {
+  private service: Service;
+
+  /**
+   * These are just used to create a working example
+   * You should implement your own code to track the state of your accessory
+   */
+  private inverterStates = {
+    serial: this.accessory.context.device.serialNumber,
+    On: this.accessory.context.device.solar.power > 0,
+    batteryPercent: this.accessory.context.device.battery.percent,
+    batteryPower: this.accessory.context.device.battery.power,
+    arrayPower: this.accessory.context.device.solar.power,
+    gridPower: this.accessory.context.device.grid.power,
+  };
+
+  constructor(
+    private readonly platform: GivEnergyPlugin,
+    private readonly accessory: PlatformAccessory,
+  ) {
+
+    // set accessory information
+    this.platform.log.debug('Setting accessory information:' + JSON.stringify(this.accessory.context.device));
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'GivEnergy')
+      .setCharacteristic(this.platform.Characteristic.Model, this.accessory.context.device.model)
+      .setCharacteristic(this.platform.Characteristic.Name, this.accessory.context.device.serialNumber)
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, this.accessory.context.device.serialNumber);
+
+    this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
+    this.service.getCharacteristic(this.platform.Characteristic.On).onGet(this.getInverterOn.bind(this));
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName);
+    this.service.setCharacteristic(this.platform.Characteristic.ConfiguredName, accessory.context.device.displayName);
+    const gridExportService = this.accessory.getService('Grid Export') ||
+      this.accessory.addService(this.platform.Service.LightSensor, 'Grid Export', 'Grid-Export');
+    gridExportService.setCharacteristic(this.platform.Characteristic.ConfiguredName, 'GridExport');
+
+    const gridImportService = this.accessory.getService('Grid Import') ||
+      this.accessory.addService(this.platform.Service.LightSensor, 'Grid Import', 'Grid-Import');
+    gridImportService.setCharacteristic(this.platform.Characteristic.ConfiguredName, 'GridImport');
+
+    const solarService = this.accessory.getService('Solar') ||
+      this.accessory.addService(this.platform.Service.LightSensor, 'Solar', 'Solar');
+    solarService.setCharacteristic(this.platform.Characteristic.ConfiguredName, 'Solar');
+
+    const batteryService = this.accessory.getService('Battery') ||
+      this.accessory.addService(this.platform.Service.Battery, 'Battery', 'Battery');
+    batteryService.setCharacteristic(this.platform.Characteristic.ConfiguredName, 'Battery');
+
+    gridExportService.name = 'Grid Export';
+
+    gridImportService.name = 'Grid Import';
+
+    solarService.name = 'Solar';
+
+    batteryService.name = 'Battery';
+
+    gridImportService.getCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel)
+      .onGet(this.getGridImport.bind(this));
+
+    gridExportService.getCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel)
+      .onGet(this.getGridExport.bind(this));
+
+    solarService.getCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel)
+      .onGet(this.getSolar.bind(this));
+
+    batteryService.getCharacteristic(this.platform.Characteristic.BatteryLevel)
+      .onGet(this.getBatteryPercent.bind(this));
+
+    batteryService.getCharacteristic(this.platform.Characteristic.ChargingState)
+      .onGet(this.getChargeState.bind(this));
+
+    batteryService.getCharacteristic(this.platform.Characteristic.StatusLowBattery)
+      .onGet(this.handleStatusLowBatteryGet.bind(this));
+
+
+    setInterval(() => {
+      axios.get(`https://api.givenergy.cloud/v1/inverter/${this.inverterStates.serial}/system-data/latest`, {headers: {
+        'Authorization': 'Bearer ' + this.platform.config.API_KEY,
+        'accept': 'application/json',
+      }, timeout: 5000}).then(response => {
+        this.inverterStates.On = response.data.data.solar.power > 0;
+        this.inverterStates.batteryPercent = response.data.data.battery.percent;
+        this.inverterStates.batteryPower = response.data.data.battery.power;
+        this.inverterStates.arrayPower = response.data.data.solar.power;
+        this.inverterStates.gridPower = response.data.data.grid.power;
+      }).catch(error => {
+        this.platform.log.error(error);
+      });
+      this.service.updateCharacteristic(this.platform.Characteristic.On, this.inverterStates.On);
+      batteryService.updateCharacteristic(this.platform.Characteristic.ChargingState,
+        this.inverterStates.batteryPower < 0 ? this.platform.Characteristic.ChargingState.CHARGING :
+          this.platform.Characteristic.ChargingState.NOT_CHARGING);
+      batteryService.updateCharacteristic(this.platform.Characteristic.BatteryLevel, this.inverterStates.batteryPercent);
+      gridImportService.updateCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel,
+        this.inverterStates.gridPower < 0 ? -1 * this.inverterStates.gridPower : 0.0001);
+      gridExportService.updateCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel,
+        this.inverterStates.gridPower > 0 ? this.inverterStates.gridPower : 0.0001);
+      solarService.updateCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel,
+        this.inverterStates.arrayPower > 0 ? this.inverterStates.arrayPower : 0.0001);
+      this.platform.log.debug('Updating data:', JSON.stringify(this.inverterStates));
+      this.platform.log.debug('Solar power:', this.inverterStates.arrayPower);
+    }, 60000);
+  }
+
+
+
+  async getBatteryPercent() {
+    return this.inverterStates.batteryPercent;
+  }
+
+  async handleStatusLowBatteryGet() {
+    this.platform.log.debug('Triggered GET StatusLowBattery');
+    return this.inverterStates.batteryPercent > 30 ?
+      this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
+      this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+  }
+
+  async getChargeState(){
+    this.platform.log.debug('Triggered GET ChargingState');
+    return this.inverterStates.batteryPower < 0 ?
+      this.platform.Characteristic.ChargingState.CHARGING :
+      this.platform.Characteristic.ChargingState.NOT_CHARGING;
+  }
+
+  async getGridImport() {
+    this.platform.log.debug('Triggered GET Grid import level');
+    return this.inverterStates.gridPower < 0 ? -1 * this.inverterStates.gridPower : 0.0001;
+  }
+
+  async getGridExport() {
+    this.platform.log.debug('Triggered GET Grid export level');
+    return this.inverterStates.gridPower > 0 ? this.inverterStates.gridPower : 0.0001;
+  }
+
+  async getSolar() {
+    this.platform.log.debug('Triggered GET Grid export level');
+    return this.inverterStates.arrayPower > 0 ? this.inverterStates.arrayPower : 0.0001;
+  }
+
+  async getInverterOn() {
+    this.platform.log.debug('Triggered GET InverterOn');
+    return this.inverterStates.On;
+  }
+
+}
+
